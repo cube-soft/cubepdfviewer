c348d4cdfdff59042f989edb5cd717df6df42d72
clown <tt.clown@gmail.com>
2010-11-26 23:06:26 +0900

    refactoring.

---
+++ cubepdf-viewer/VersionDialog.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Cube
+{
+    partial class VersionDialog
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OKButton = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.CopyrightLabel = new System.Windows.Forms.Label();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.CubePDFLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(193, 88);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 4;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TitleLabel.Location = new System.Drawing.Point(56, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(115, 13);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "CubePDF Viewer";
+            // 
+            // CopyrightLabel
+            // 
+            this.CopyrightLabel.AutoSize = true;
+            this.CopyrightLabel.Location = new System.Drawing.Point(57, 41);
+            this.CopyrightLabel.Name = "CopyrightLabel";
+            this.CopyrightLabel.Size = new System.Drawing.Size(151, 12);
+            this.CopyrightLabel.TabIndex = 2;
+            this.CopyrightLabel.Text = "Copyright(C) 2010 CubeSoft.";
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Location = new System.Drawing.Point(57, 29);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(44, 12);
+            this.VersionLabel.TabIndex = 1;
+            this.VersionLabel.Text = "Version";
+            // 
+            // CubePDFLinkLabel
+            // 
+            this.CubePDFLinkLabel.AutoSize = true;
+            this.CubePDFLinkLabel.Location = new System.Drawing.Point(57, 63);
+            this.CubePDFLinkLabel.Name = "CubePDFLinkLabel";
+            this.CubePDFLinkLabel.Size = new System.Drawing.Size(211, 12);
+            this.CubePDFLinkLabel.TabIndex = 3;
+            this.CubePDFLinkLabel.TabStop = true;
+            this.CubePDFLinkLabel.Text = "http://www.cube-soft.jp/cubepdfviewer/";
+            this.CubePDFLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.CubePDFLinkLabel_LinkClicked);
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Location = new System.Drawing.Point(12, 29);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.LogoPictureBox.TabIndex = 5;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // VersionDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 123);
+            this.Controls.Add(this.LogoPictureBox);
+            this.Controls.Add(this.CubePDFLinkLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.CopyrightLabel);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.TitleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VersionDialog";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "バージョン情報";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label CopyrightLabel;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.LinkLabel CubePDFLinkLabel;
+        private System.Windows.Forms.PictureBox LogoPictureBox;
+
+    }
+}
---
+++ cubepdf-viewer/VersionDialog.cs
@@ -0,0 +1,64 @@
+﻿/* ------------------------------------------------------------------------- */
+/*
+ *  VersionDialog.cs
+ *
+ *  Copyright (c) 2010 CubeSoft Inc. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see < http://www.gnu.org/licenses/ >.
+ *
+ *  Last-modified: Fri 26 Nov 2010 22:44:00 JST
+ */
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Windows.Forms;
+
+namespace Cube {
+    /* --------------------------------------------------------------------- */
+    /// VersionDialog
+    /* --------------------------------------------------------------------- */
+    public partial class VersionDialog : Form {
+        /* ----------------------------------------------------------------- */
+        /// Constructor
+        /* ----------------------------------------------------------------- */
+        public VersionDialog() {
+            InitializeComponent();
+            var registry = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(REG_ROOT, false);
+            var version = registry.GetValue(REG_VERSION, "unknown");
+            this.VersionLabel.Text = "version " + version;
+            LogoPictureBox.Image = Cube.Properties.Resources.cubepdf_viewer.ToBitmap();
+        }
+
+        /* ----------------------------------------------------------------- */
+        /// CubePDFLinkLabel_LinkClicked
+        /* ----------------------------------------------------------------- */
+        private void CubePDFLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            System.Diagnostics.Process.Start(CubePDFLinkLabel.Text);
+        }
+
+        /* ----------------------------------------------------------------- */
+        /// OKButton_Click
+        /* ----------------------------------------------------------------- */
+        private void OKButton_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        /* ----------------------------------------------------------------- */
+        //  定数定義
+        /* ----------------------------------------------------------------- */
+        #region constant variables
+        const string REG_ROOT       = @"Software\CubeSoft\CubePDF Viewer";
+        const string REG_VERSION    = "Version";
+        #endregion
+    }
+}
